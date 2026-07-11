d64083755c9ceff3b0cf3432efd49c7c0ce0a1a5
sagilityph <sagilityassessment@gmail.com>
2026-07-11 19:57:15 +0800

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,99 @@
+const SITE = "qc";
+
+import { db } from "./firebase.js";
+import {
+    ref,
+    set,
+    get,
+    push
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+
+const g = document.getElementById("grid");
+
+async function loadSeats() {
+
+    const snapshot = await get(
+        ref(db, `locations/${SITE}/seats`)
+    );
+
+    let seats = {};
+
+    if (snapshot.exists()) {
+        seats = snapshot.val();
+    }
+
+    g.innerHTML = "";
+
+    for (let i = 1; i <= 20; i++) {
+
+        let v = seats[i] || 0;
+
+        g.innerHTML += `
+        <div class="card">
+            <h3>Seat ${i}</h3>
+
+            <input
+                id="seat${i}"
+                value="${v}"
+                onchange="saveSeat(${i}, this.value)"
+            >
+
+            <button
+                id="b${i}"
+                onclick="callSeat(${i})"
+            >
+                CALL
+            </button>
+        </div>
+        `;
+    }
+}
+
+window.saveSeat = async function(seat, value){
+
+    await set(
+        ref(
+            db,
+            `locations/${SITE}/seats/${seat}`
+        ),
+        value || 0
+    );
+
+};
+
+window.callSeat = async function(seat){
+
+    const seatSnapshot = await get(
+        ref(
+            db,
+            `locations/${SITE}/seats/${seat}`
+        )
+    );
+
+    let id = 0;
+
+    if (seatSnapshot.exists()) {
+        id = seatSnapshot.val();
+    }
+
+    await push(
+        ref(
+            db,
+            `locations/${SITE}/queue`
+        ),
+        {
+            seat: seat,
+            id: id,
+            timestamp: Date.now()
+        }
+    );
+
+    const btn = document.getElementById(
+        "b" + seat
+    );
+
+    btn.className = "called";
+    btn.innerHTML = "CALLED ✓";
+};
+
+loadSeats();
